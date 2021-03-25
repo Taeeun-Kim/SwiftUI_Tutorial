170257f75d15e4b600e16fd5655a9f75248cf6c8
Taeeun Kim <taeeun.kim@outlook.com>
2021-03-25 17:15:51 +0100

    Update code

--- Calculator/Calculator/ContentView.swift
+++ Calculator/Calculator/ContentView.swift
@@ -7,78 +7,59 @@
 
 import SwiftUI
 
-enum CalcButton: String{
-    case one = "1"
-    case two = "2"
-    case three = "3"
-    case four = "4"
-    case five = "5"
-    case six = "6"
-    case seven = "7"
-    case eight = "8"
-    case nine = "9"
-    case zero = "0"
-    case add = "+"
-    case subtract = "-"
-    case divide = "/"
-    case multiply = "x"
-    case equal = "="
-    case clear = "AC"
-    case decimal = "."
-    case percent = "%"
-    case negative = "-/+"
+enum CalButton {
+    case one
+    case two
+    case three
+    case four
+    case five
+    case six
+    case seven
+    case eight
+    case nine
+    case zero
+    case add
+    case subtract
+    case divide
+    case multiply
+    case equal
+    case clear
+    case decimal
+    case percent
+    case negative
 }
 
 struct ContentView: View {
     
-    let buttons: [[CalcButton]] = [
-        [.clear, .negative, .percent, .divide],
-        [.seven, .eight, .nine, .multiply],
-        [.four, .five, .six, .subtract],
-        [.one, .two, .three, .add],
-        [.zero, .decimal, .equal],
+    let buttons: [[CalButton]] = [
+        [.seven, .eight, .nine] // 0 0, 0 1, 0 2
     ]
     
     var body: some View {
         ZStack{
             Color.black.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            
-            VStack{
-                // Text display on a calculator
-                HStack{
-                    Spacer()
-                    Text("0")
-                        .bold()
-                        .font(.system(size: 52))
-                        .foregroundColor(.white)
-                }
-                .padding()
-                
-                // Buttons
-                    ForEach(buttons, id: \.self){ row in
-                        HStack{
-                            ForEach(row, id: \.self){ item in
-                                Button(action: {
-                                    
-                                }, label: {
-                                    Text(item.rawValue)
-                                        .font(.system(size: 32))
-                                        .frame(width: 70, height: 70)
-                                        .background(Color.orange)
-                                        .foregroundColor(.white)
-                                        .cornerRadius(35)
-                                })
-                            }
-                        }
-                    }
+            HStack{
+                Spacer()
+                Text("555,0")
+                    .foregroundColor(.white)
+                    .font(.system(size: 80))
             }
+            .padding()
+            
+            // Buttons
+            ForEach(buttons, id: \.self){ row in
+                // row는 buttons의 값들을 차례대로 받음
+                ForEach(row, id: \.self){ item in
+                    // item은 row의 값들을 차례대로 받음
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        Text(item.rawValue)
+                    })
+                }
+                
+            }
+            
         }
     }
-    
-    func buttonWidth(item: CalcButton) -> CGFloat {
-        return (UIScreen.main.bounds.width - (5*12))/4
-    }
-    
 }
 
 struct ContentView_Previews: PreviewProvider {
