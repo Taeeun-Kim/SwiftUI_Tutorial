5622696d48f8f25f96597f942b2cad1528942f5d
Taeeun Kim <taeeun.kim@outlook.com>
2021-03-25 19:05:00 +0100

    Lecture 1 from Stanford.edu

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -9,20 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack(content: {
-            ForEach(0..<4, content: { index in
-                ZStack(content: {
-                    RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                    RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
-                    Text("👻")
-                })
-            })
-        })
+        HStack { // spacing: 0,
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: false)
+            }
+        }
             .padding()
             .foregroundColor(.orange)
             .font(Font.largeTitle)
     }
 }
+
+struct CardView: View {
+    var isFaceUp: Bool
+    
+    var body: some View{
+        ZStack {
+            if isFaceUp{
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("👻")
+            } else{
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
