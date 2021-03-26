2bc1c05f03ecd4034e54e25dae09ead3158adfc0
Taeeun Kim <taeeun.kim@outlook.com>
2021-03-26 22:37:54 +0100

    Update tutorial

---
+++ SwiftUI_Stack_Practice/SwiftUI_Stack_Practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Stack_Practice
+//
+//  Created by Taeeun Kim on 26.03.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Stack_Practice/SwiftUI_Stack_Practice/MyProjectCard.swift
@@ -0,0 +1,22 @@
+//
+//  MyProjectCard.swift
+//  SwiftUI_Stack_Practice
+//
+//  Created by Taeeun Kim on 26.03.21.
+//
+
+import SwiftUI
+
+struct MyProjectCard: View {
+    var body: some View{
+        VStack{
+            Text("Taeeun Jessica Estelle Project")
+        }
+    }
+}
+
+struct MyProjectCard_Previews: PreviewProvider {
+    static var previews: some View {
+        MyProjectCard()
+    }
+}
