1cf328cdcb011a8eea8376640437ec4a7474dd27
Taeeun Kim <taeeun.kim@outlook.com>
2021-03-26 11:10:53 +0100

    Update code

--- Memorize/Memorize/EmojiMemoryGame.swift
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -7,15 +7,23 @@
 
 import SwiftUI
 
-func createCardContent(pairIndex: int) -> String {
-    
-}
+//func createCardContent(pairIndex: Int) -> String {
+//    return "🤗"
+//} 이걸 바로 밑에 cardContentFactory: 인라인에서 만들꺼임, 즉 클로저!
 
 class EmojiMemoryGame {
-    private(set) var model: MemoryGame<String> =
-        MemoryGame<String>(numberOfPairsOfCards: 2, cardContentFactory: <#T##(Int) -> String#>)
-    // glass door, they can only see
-    // model은 MemoryGame<제너릭>의 불러오기 위함
+    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> { // 스태틱으로 선엄함으로 인해, 함수를 벗어나도 사용 가능
+        let emojis = ["😬","🎃","👾"] // index[0], index[1]
+        return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in
+            return emojis[pairIndex]
+        }
+    }
+    /* 즉 이건, init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent)
+       여기에서 numberOf..는 그대로, 뒷부분 cardContentFac..은 클로저로 대체
+       glass door, they can only see
+       model은 MemoryGame<제너릭>의 불러오기 위함 */
     
     // MARK: - Access to the model
     var cards: Array<MemoryGame<String>.Card>{
--- Memorize/Memorize/MemoryGame.swift
+++ Memorize/Memorize/MemoryGame.swift
@@ -14,18 +14,24 @@ struct MemoryGame<CardContent> { // 제너릭
         print("card chosen: \(card)")
     }
     
+    // initialize
     init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) { // no return, just initialize
         cards = Array<Card>() // empty array cards
         for pairIndex in 0..<numberOfPairsOfCards {
             let content = cardContentFactory(pairIndex) // content: cardContent
-            cards.append(Card(isFaceUp: false, inMatched: false, content: <#T##CardContent#>))
-            cards.append(Card(isFaceUp: false, inMatched: false, content: <#T##CardContent#>))
+            cards.append(Card(content: content, id: pairIndex*2))
+            cards.append(Card(content: content, id: pairIndex*2+1))
+            // pairIndex는 0 1 2 3 .. 이렇게 늘어나기에, 한개의 pairIndex에 두개의 카드를 추가해야하니,
+            // 예를 들어,
+            // 인덱스 0이면, 0*2=0, 0*2+1=1
+            // 인덱스 1이면, 1*2=2, 1*2+1=3  즉 순서대로 0, 1, 2, 3 .. 순서에 맞게 id 대입이 됨
         }
     }
     
-    struct Card {
-        var isFaceUp: Bool
-        var inMatched: Bool
+    struct Card: Identifiable {
+        var isFaceUp: Bool = true
+        var inMatched: Bool = false
         var content: CardContent // 숫자일 수도 있고, 이모지일 수도 있고, 스트링일 수도 있고, 고로 제너릭 선언 필요
+        var id: Int
     }
 }
